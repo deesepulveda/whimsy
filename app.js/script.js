ed5519fc3ec83aa7409f753c084d5357fda253e4
dee chao <chaoserady@gmail.com>
2023-02-12 03:04:05 -0600

    updated files

--- app.js/script.js
+++ app.js/script.js
@@ -2,7 +2,14 @@
 
 const sideLiDrivers = document.querySelector(".side_li_items_drivers");
 const driversUl = document.querySelector(".drivers_ul");
+const sideLi = document.querySelectorAll(".side_li_items");
 
 sideLiDrivers.addEventListener("click", () => {
   driversUl.classList.add("showList");
+});
+
+sideLi.forEach((si) => {
+  si.addEventListener("click", () => {
+    console.log("Clicked");
+  });
 });
