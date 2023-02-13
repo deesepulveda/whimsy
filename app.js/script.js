592788261fa2cd2bf099cf1f67abb23f2ddcbaa0
dee chao <chaoserady@gmail.com>
2023-02-12 23:34:05 -0600

    updated js function to list

--- app.js/script.js
+++ app.js/script.js
@@ -1,15 +1,22 @@
 "use strict";
 
 const sideLiDrivers = document.querySelector(".side_li_items_drivers");
-const driversUl = document.querySelector(".drivers_ul");
-const sideLi = document.querySelectorAll(".side_li_items");
-
-sideLiDrivers.addEventListener("click", () => {
-  driversUl.classList.add("showList");
-});
+const driversDash = document.querySelector(".drivers_dashboard");
+const sideLi = document.querySelectorAll(".side_li");
+const dashBoard = document.querySelectorAll(".dashboard");
 
 sideLi.forEach((si) => {
   si.addEventListener("click", () => {
-    console.log("Clicked");
+    let current = "";
+    current = si.getAttribute("id");
+
+    dashBoard.forEach((db) => {
+      if (db.classList.contains(current)) {
+        db.classList.remove("hideList");
+        db.classList.add("showList");
+      } else {
+        db.classList.add("hideList");
+      }
+    });
   });
 });
