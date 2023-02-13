ff5188dc5467e8b766ff68a3306a3668781b2259
dee chao <chaoserady@gmail.com>
2023-02-13 01:56:07 -0600

    updated dashboard sections and uploaded first card

--- app.js/script.js
+++ app.js/script.js
@@ -4,12 +4,16 @@ const sideLiDrivers = document.querySelector(".side_li_items_drivers");
 const driversDash = document.querySelector(".drivers_dashboard");
 const sideLi = document.querySelectorAll(".side_li");
 const dashBoard = document.querySelectorAll(".dashboard");
+const cards = document.querySelector(".cards");
+
+// Side Menu List Functions
 
 sideLi.forEach((si) => {
   si.addEventListener("click", () => {
     let current = "";
     current = si.getAttribute("id");
 
+    // Target Dashboards after Side List Clicked
     dashBoard.forEach((db) => {
       if (db.classList.contains(current)) {
         db.classList.remove("hideList");
@@ -20,3 +24,7 @@ sideLi.forEach((si) => {
     });
   });
 });
+
+// cards.addEventListener("click", () => {
+//   cards.classList.toggle("acresYard");
+// });
